9a67606b76360883ccbf54cc6193f538636374a7
Mazen Sadek <mazend2242@gmail.com>
2024-11-04 00:03:48 +0200

    fixed overflow issues

---
+++ main.js
@@ -0,0 +1,31 @@
+// window.addEventListener("resize", function () {
+//     // Get the current width of the window
+//     const windowWidth = window.innerWidth;
+//     // Select all elements with the class "nav__link"
+//     const elements = document.querySelectorAll(".nav__link");
+  
+//     // Specify the width at which you want to remove the elements
+//     const targetWidth = 880; // Example width
+  
+//     if (windowWidth <= targetWidth) {
+//       // Loop through the NodeList and remove each element from the DOM
+//       elements.forEach(element => {
+//         element.remove();
+//       });
+//     }
+//   });
+
+//   window.addEventListener("load", function () {
+//     const windowWidth = window.innerWidth;
+//     const elements = document.querySelector(".nav__link");
+//     const targetWidth = 880;
+  
+//     if (windowWidth <= targetWidth) {
+//       elements.forEach(element => {
+//         element.remove();
+//       });
+//     }
+// });
+
+//   const elementsTest = document.querySelectorAll(".nav__link");
+//   console.log(elements);
